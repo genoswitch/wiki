b3215de4beaeb9d62e459872222a8497c909b482
James Cahill <jcahill773@gmail.com>
2023-06-21 19:27:10 +0100

    Allow gatsby to transform/load yml files into graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,20 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-transformer-yaml",
+      options: {
+        typeName: "TeamMember"
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./data/",
+      }
+    }
+  ],
 }
 
 export default config

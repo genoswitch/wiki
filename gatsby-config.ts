58f018f9e67d9e6149129748a188bd4e038a8d38
James Cahill <jcahill773@gmail.com>
2023-06-23 15:41:47 +0100

    (gatsby-config): Add pathPrefix with default option

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,6 +6,7 @@ const config: GatsbyConfig = {
     assetBasePath: (process.env.ASSET_BASE_PATH || "https://static.igem.wiki/teams/4642/wiki/")
   
   },
+  pathPrefix: (process.env.PATH_PREFIX || "/city-of-london-uk"),
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen

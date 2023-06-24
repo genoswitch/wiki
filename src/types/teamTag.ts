02691759e542587c152af77fc6b1a5c7261878b4
James Cahill <jcahill773@gmail.com>
2023-06-24 18:33:27 +0100

    (types): Add TeamTag class

---
+++ src/types/teamTag.ts
@@ -0,0 +1,15 @@
+export default class TeamTag {
+	name: string;
+	colour: string;
+	constructor(name: string, colour: string) {
+		this.name = name;
+
+		// Check if bgcol is a hex value without the preceding hash (#). If so, add a '#'.
+		if (/^[0-9A-F]{6}$/i.test(colour)) {
+			// Regex based on https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
+			colour = `#${colour}`;
+		}
+
+		this.colour = colour;
+	}
+}

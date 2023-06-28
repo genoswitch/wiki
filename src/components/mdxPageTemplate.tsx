f862e72d1f7678ba9f50539d83cc35f6d64e2a24
James Cahill <jcahill773@gmail.com>
2023-06-28 19:34:22 +0100

    (components/mdxPageTemplate): Add prop ts type

--- src/components/mdxPageTemplate.tsx
+++ src/components/mdxPageTemplate.tsx
@@ -2,7 +2,14 @@ import * as React from "react";
 import NavBar from "./navbar";
 import { Container } from "@mui/material";
 
-const MdxPageTemplate = ({ children }) => {
+// Define a custom type so TypeScript understands what is being passed to the function
+// Without a type, the "any" type is implicitly set.
+// With a defined type, we can get autocomplete and whatnot from our IDE as well.
+type MdxPageTemplatePropTypes = {
+	children: React.JSX.Element[];
+};
+
+const MdxPageTemplate = ({ children }: MdxPageTemplatePropTypes) => {
 	return (
 		<>
 			<NavBar />

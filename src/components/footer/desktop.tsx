c525befa702f3cf4b7187bfa8d967a994f1f250a
James Cahill <jcahill773@gmail.com>
2023-07-02 15:42:47 +0100

    (components/footer): Experiment with desktop footer layout based on ddc629d3dcb64f3947dec600d8635ef69901d2fd

--- src/components/footer/desktop.tsx
+++ src/components/footer/desktop.tsx
@@ -1,8 +1,74 @@
+import { Button, Grid, Paper, Typography } from "@mui/material";
+
+import { graphql, useStaticQuery } from "gatsby";
+
 import * as React from "react";
 
-// Placeholder footer
+export const query = graphql`
+    query FooterData {
+        site {
+            siteMetadata {
+            sha
+            assetBasePath
+        }
+    }
+}
+`;
+
+
+const sponsors = [
+    {
+        name: "Hennigan",
+        url: "https://static.igem.wiki/teams/4642/wiki/icon.png",
+    },
+    {
+        name: "John Carpenter Club",
+        url: "https://static.igem.wiki/teams/4642/wiki/logos/john-carpenter-club-transparent.png"
+    },
+    {
+        name: "Roger Counter Foundation",
+        url: "https://static.igem.wiki/teams/4642/wiki/logos/roger-counter-foundation-transparent.png",
+    }, {
+        name: "Barbers Livery Company",
+        url: "https://static.igem.wiki/teams/4642/wiki/logos/barbers-company-barber-surgeons-crest.png",
+    }
+]
+
 const DesktopFooter = () => {
-    return (<div>DESKTOP: Hello, world!</div>)
+    const data = useStaticQuery(query);
+    const assetPath = data.site.siteMetadata.assetBasePath;
+    const longSha = data.site.siteMetadata.sha;
+    const shortSha = longSha.substring(0, 8);
+
+    return (
+        <>
+            <Paper sx={{ backgroundColor: "#0a1628", color: "white", padding: 3 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Grid container spacing={2}>
+                            {sponsors.map(sponsor => {
+                                return (<Grid xs={2} sx={{ display: "flex", alignItems: "center" }}>
+                                    <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={sponsor.url} />
+                                </Grid>)
+                            })}
+                        </Grid>
+                    </Grid>
+                    <Grid xs={8} display="flex" alignItems="center">
+                        {/** Source Code Button */}
+                        <Button variant="contained" target="_blank" href="https://gitlab.igem.org/2023/city-of-london-uk">Source Code</Button>
+                        {/** SHA Text / Button */}
+                        <Typography sx={{ paddingLeft: 2, color: "white" }}>(SHA: <Button variant="text" target="_blank" href={`https://gitlab.igem.org/2023/city-of-london-uk/-/commit/${longSha}`}>{shortSha}</Button>)</Typography>
+                    </Grid>
+                    <Grid xs={2}>
+                        <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={assetPath + "logos/clsg-transparent.png"} />
+                    </Grid>
+                    <Grid xs={2}>
+                        <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={assetPath + "logos/clsb-transparent.png"} />
+                    </Grid>
+                </Grid>
+            </Paper >
+        </>
+    )
 }
 
 export default DesktopFooter;

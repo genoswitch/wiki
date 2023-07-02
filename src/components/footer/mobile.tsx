0f5995d20054f6f456112a5cdf563d79f1d68232
James Cahill <jcahill773@gmail.com>
2023-07-02 19:52:00 +0100

    (components/footer): Initial mobile footer design
    Padding issues on certain elements.

--- src/components/footer/mobile.tsx
+++ src/components/footer/mobile.tsx
@@ -1,8 +1,61 @@
 import * as React from "react";
 
-// Placeholder footer
+import { graphql, useStaticQuery } from "gatsby";
+
+import { Grid, Paper } from "@mui/material";
+
+import SponsorLogos from "./components/sponsorLogos";
+import SchoolLogos from "./components/schoolLogos";
+import SourceAndSha from "./components/sourceAndSha";
+import Copyright from "./components/copyright";
+
+export const query = graphql`
+    query FooterData {
+        site {
+            siteMetadata {
+                sha
+                assetBasePath
+            }
+        }
+        allSponsorYaml {
+            nodes {
+                name
+                logoPath
+            }
+        }
+    }
+`;
+
+
 const MobileFooter = () => {
-    return (<div>MOBILE: Hello, world!</div>)
+    const data = useStaticQuery(query);
+    const assetPath = data.site.siteMetadata.assetBasePath;
+    const longSha = data.site.siteMetadata.sha;
+    const shortSha = longSha.substring(0, 8);
+
+    return (
+        <>
+            <Paper sx={{ backgroundColor: "#0a1628", color: "white" }}>
+                {/** Root grid. Size: 12 columns (default) */}
+                <Grid container spacing={2}>
+
+                    {/** Desktop: Top left (nothing yet) */}
+
+                    {/** Sponsors */}
+                    <SponsorLogos containerSx={{}} elementSx={{ display: "flex", alignItems: "center" }} size={4} nodes={data.allSponsorYaml.nodes} assetPath={assetPath} />
+
+                    {/** School Logos */}
+                    <SchoolLogos size={6} assetPath={assetPath} />
+
+                    {/** Source code button and SHA */}
+                    <SourceAndSha longSha={longSha} shortSha={shortSha} />
+
+                    {/** Copyright */}
+                    <Copyright />
+                </Grid>
+            </Paper>
+        </>
+    )
 }
 
 export default MobileFooter;

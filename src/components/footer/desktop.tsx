fa55a6fb4b8a31be507a49cb9d448a2dc686a12b
James Cahill <jcahill773@gmail.com>
2023-07-02 15:50:07 +0100

    (components/footer): Set precise sizes to allow for multi-line positioning

--- src/components/footer/desktop.tsx
+++ src/components/footer/desktop.tsx
@@ -43,26 +43,33 @@ const DesktopFooter = () => {
     return (
         <>
             <Paper sx={{ backgroundColor: "#0a1628", color: "white", padding: 3 }}>
+                {/** 12 columns (default) */}
                 <Grid container spacing={2}>
-                    <Grid item xs={8}>
-                        <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        TOP LEFT
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Grid container spacing={2} sx={{ justifyContent: "right" }}>
                             {sponsors.map(sponsor => {
-                                return (<Grid xs={2} sx={{ display: "flex", alignItems: "center" }}>
-                                    <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={sponsor.url} />
-                                </Grid>)
+                                return (
+                                    <Grid xs={2} sx={{ display: "flex", alignItems: "center" }}>
+                                        <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={sponsor.url} />
+                                    </Grid>
+                                )
                             })}
                         </Grid>
                     </Grid>
-                    <Grid xs={8} display="flex" alignItems="center">
+                    <Grid xs={8} display="flex" alignItems="end">
                         {/** Source Code Button */}
                         <Button variant="contained" target="_blank" href="https://gitlab.igem.org/2023/city-of-london-uk">Source Code</Button>
                         {/** SHA Text / Button */}
                         <Typography sx={{ paddingLeft: 2, color: "white" }}>(SHA: <Button variant="text" target="_blank" href={`https://gitlab.igem.org/2023/city-of-london-uk/-/commit/${longSha}`}>{shortSha}</Button>)</Typography>
                     </Grid>
-                    <Grid xs={2}>
+
+                    <Grid item xs={2}>
                         <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={assetPath + "logos/clsg-transparent.png"} />
                     </Grid>
-                    <Grid xs={2}>
+                    <Grid item xs={2}>
                         <img style={{ maxWidth: "100%", maxHeight: "100%" }} src={assetPath + "logos/clsb-transparent.png"} />
                     </Grid>
                 </Grid>

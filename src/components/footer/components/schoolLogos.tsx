e5a62c57c0f94cdc7ec1b12e54b2ee1f1d9069e7
James Cahill <jcahill773@gmail.com>
2023-07-03 17:57:55 +0100

    Add links, alts to school logos

--- src/components/footer/components/schoolLogos.tsx
+++ src/components/footer/components/schoolLogos.tsx
@@ -12,16 +12,22 @@ const SchoolLogos = ({ assetPath, size, containerSx }: SchoolLogoProps) => {
 	return (
 		<Grid item container spacing={0} sx={containerSx}>
 			<Grid item xs={size}>
-				<img
-					style={{ maxWidth: "100%", maxHeight: "100%" }}
-					src={assetPath + "logos/clsg-transparent.png"}
-				/>
+				<a href="https://clsg.org.uk/" target="_blank">
+					<img
+						style={{ maxWidth: "100%", maxHeight: "100%" }}
+						src={assetPath + "logos/clsg-transparent.png"}
+						alt="City of London School for Girls Logo"
+					/>
+				</a>
 			</Grid>
 			<Grid item xs={size}>
-				<img
-					style={{ maxWidth: "100%", maxHeight: "100%" }}
-					src={assetPath + "logos/clsb-transparent.png"}
-				/>
+				<a href="https://www.cityoflondonschool.org.uk/" target="_blank">
+					<img
+						style={{ maxWidth: "100%", maxHeight: "100%" }}
+						src={assetPath + "logos/clsb-transparent.png"}
+						alt="City of London School for Boys Logo"
+					/>
+				</a>
 			</Grid>
 		</Grid>
 	);

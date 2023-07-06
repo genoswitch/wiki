8d2bc3614b0292ed3f1889182402ded92ff5da36
James Cahill <jcahill773@gmail.com>
2023-07-06 19:42:28 +0100

    Use strict typing for mobile, desktop footer

--- src/components/footer/mobile.tsx
+++ src/components/footer/mobile.tsx
@@ -6,10 +6,15 @@ import SponsorLogos from "./components/sponsorLogos";
 import SchoolLogos from "./components/schoolLogos";
 import SourceAndSha from "./components/sourceAndSha";
 import Copyright from "./components/copyright";
+import { FooterProps } from "./types/footerProps";
+import { SponsorNode } from "../../types/graphql/sponsorNode";
 
-const MobileFooter = ({ data }) => {
-	const assetPath = data.site.siteMetadata.assetBasePath;
-	const longSha = data.site.siteMetadata.sha;
+// We are using the type Queries.Query in which everything is an optional.
+// Unfortunately that means we have to notNull everything
+// (We are currently assuming that the programmer had the correct query)
+const MobileFooter = ({ data }: FooterProps) => {
+	const assetPath = data!.site!.siteMetadata!.assetBasePath!;
+	const longSha = data!.site!.siteMetadata!.sha!;
 	const shortSha = longSha.substring(0, 8);
 
 	return (
@@ -27,7 +32,16 @@ const MobileFooter = ({ data }) => {
 						containerSx={{}}
 						elementSx={{ display: "flex", alignItems: "center" }}
 						size={4}
-						nodes={data.allSponsorYaml.nodes}
+						/**
+						 * TypeScript believes that Queries.SponsorYaml[]
+						 * and SponsorNode[] do not (sufficiently) overlap.
+						 * 
+						 * (SponsorNode is based on SponsorYaml.)
+						 * 
+						 * Therefore, we must cast to unknown first before
+						 * casting to SponsorNode.
+						 */
+						nodes={data!.allSponsorYaml!.nodes! as unknown as SponsorNode[]}
 						assetPath={assetPath}
 					/>
 

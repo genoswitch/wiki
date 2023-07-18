f3f076f35e4252c902d3898e96ecd1aecda591f7
James Cahill <jcahill773@gmail.com>
2023-07-18 17:47:23 +0100

    (components/seo): Add google-site-verification

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -11,6 +11,10 @@ const SEO = ({ children, title, description }: SEOProps) => {
 		<>
 			<title>{title}</title>
 			<meta name="description" content={description} />
+
+			{/** Google Webmaster Tools Site Verification so we can manage indexing on Google. */}
+			<meta name="google-site-verification" content="kd28eSB57PmSeee40SHQh9M8YaNP1jzcR7avOs0aVpA" />
+
 			{/** viewport is already set by Gatsby */}
 			{children}
 		</>

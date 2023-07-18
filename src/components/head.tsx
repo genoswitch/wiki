a495bc4dbed41313fff79ca024aca576ed817865
James Cahill <jcahill773@gmail.com>
2023-07-18 17:40:58 +0100

    (components): Add Head, SEO components

---
+++ src/components/head.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import SEO from "./seo";
+
+type HeadProps = {
+	children: React.JSX.Element;
+	title?: string;
+	description?: string;
+};
+
+export const Head = ({ children, title, description }: HeadProps) => {
+	let computedTitle = "Genoswitch";
+	if (title) {
+		computedTitle = `${title} | ${computedTitle}`;
+	}
+
+	let computedDescription = "City of London School iGEM 2023 Wiki";
+	if (description) {
+		computedDescription = description;
+	}
+
+	return (
+		<SEO title={computedTitle} description={computedDescription}>
+			{children}
+		</SEO>
+	);
+};

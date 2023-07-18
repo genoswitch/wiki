bcdc2d26d73259340b5790b32090e820d044ef43
James Cahill <jcahill773@gmail.com>
2023-07-18 17:42:14 +0100

    (head|seo): Make children optional

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 
 type SEOProps = {
-	children: React.JSX.Element;
-	title: string;
-	description: string;
+    children?: React.JSX.Element;
+    title: string;
+    description: string;
 };
 
 const SEO = ({ children, title, description }: SEOProps) => {
-	return (
-		<>
-			<title>{title}</title>
-			<meta name="description" content={description} />
-			{/** viewport is already set by Gatsby */}
-			{children}
-		</>
-	);
+    return (
+        <>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            {/** viewport is already set by Gatsby */}
+            {children}
+        </>
+    );
 };
 
 export default SEO;

b110c70863f39c4dcfbae29d2f3c3d82e9d2dabe
James Cahill <jcahill773@gmail.com>
2023-07-29 17:15:07 +0100

    Switch to a MUI nested list for child entries

--- src/components/navbar/components/mobileButtonDropdown.tsx
+++ src/components/navbar/components/mobileButtonDropdown.tsx
@@ -1,35 +1,41 @@
 import * as React from "react";
 import NavigationEntry from "../types/navigationEntry";
-import { ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Collapse, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { withPrefix } from "gatsby";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
 type MobileButtonDropdownProps = {
     entry: NavigationEntry;
 }
 
 const MobileButtonDropdown = ({ entry }: MobileButtonDropdownProps) => {
-    const [isOpen, setIsOpen] = React.useState<Boolean>(false);
+    const [open, setOpen] = React.useState(false);
 
-    const onClick = () => {
-        setIsOpen(!isOpen);
+    const handleClick = () => {
+        setOpen(!open);
     }
 
     return (
         <>
             <ListItem key={entry.name} disablePadding>
-                <ListItemButton sx={{ textAlign: "center" }} onClick={onClick}>
+                <ListItemButton sx={{ textAlign: "center" }} onClick={handleClick}>
                     <ListItemText primary={entry.name} />
+                    {open ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
             </ListItem>
-            {entry.entries.map(subEntry => (
-                <>
-                    {isOpen && <ListItem key={subEntry.name} disablePadding>
-                        <ListItemButton sx={{ textAlign: "center" }} href={withPrefix(subEntry.slug!)}>
-                            <ListItemText primary={subEntry.name} />
-                        </ListItemButton>
-                    </ListItem>}
-                </>
-            ))}
+            {/** MUI Nested List */}
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    {entry.entries.map(subEntry => (
+                        <ListItem key={subEntry.name} disablePadding>
+                            <ListItemButton sx={{ textAlign: "center" }} href={withPrefix(subEntry.slug!)}>
+                                <ListItemText primary={subEntry.name} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Collapse>
+
         </>
     )
 }

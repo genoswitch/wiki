32f572935556e64d8a4b725f2698c8d94714f5b4
James Cahill <jcahill773@gmail.com>
2023-08-04 16:34:59 +0000

    Initial Hardware page, support for references in MDX pages using IEEE style (#69)
    * Initial support for references/citations using IEEE style
    
    * (pages/mdx): Add initial hardware page with references
    
    * (components/mdxPageTemplate): Show nothing when no references found
    
    * (components/referenceProvider): Use a divider at the start of the component
    
    * Add optional volume attribute
    
    * Add hardware to navbar
    
    * (lint): Run prettier against ed366af330d6ca4dbdce658af090de808b0642ce
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/components/referenceProvider.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+import { Divider } from "@mui/material";
+
+type ReferenceProviderProps = {
+	references: readonly Queries.Maybe<Queries.MdxFrontmatterReferences>[] | undefined;
+};
+
+const ReferenceProvider = ({ references }: ReferenceProviderProps) => {
+	if (!references) {
+		throw new Error("Null references passed to ReferenceProvider!");
+	} else {
+		return (
+			<>
+				<Divider variant="middle" />
+				<h4>References</h4>
+				{references.map((ref: Queries.Maybe<Queries.MdxFrontmatterReferences>) => {
+					// 1. ref number, author, title, doi
+					if (
+						ref?.number &&
+						ref?.author &&
+						ref?.title &&
+						ref?.journal &&
+						ref?.published_date &&
+						ref?.doi
+					) {
+						return (
+							<span id={`ref_note-${ref?.number}`}>
+								{ref?.number}. <a href={`#ref_${ref?.number}`}>^</a> {ref?.author}, "{ref?.title}",{" "}
+								<i>{ref?.journal}</i>
+								{ref?.volume ? `, ${ref.volume}` : ""}, {ref?.published_date}, Available:{" "}
+								<a
+									target="_blank"
+									href={`https://doi.org/${ref?.doi}`}
+								>{`https://doi.org/${ref?.doi}`}</a>
+							</span>
+						);
+					}
+				})}
+			</>
+		);
+	}
+};
+
+export default ReferenceProvider;

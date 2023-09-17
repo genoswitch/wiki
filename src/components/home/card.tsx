3340e20a3848eade667399314d4041cdbd04b5eb
Rishbong <r.das@cityoflondonschool.org.uk>
2023-09-17 12:12:14 +0100

    added front page stuff, cards, bioGang image is a placeholder before I upload stuff to the wiki

--- src/components/home/card.tsx
+++ src/components/home/card.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import "./card.css";
 
-const Card = () => {
+function Card(props: any) {
     return (
         <div className="card">
-            <h1>TITLE</h1>
-            <p>a bunch of text </p>
+
+            {/* <img src="" className="cardImage"/> */}
+            <img src={props.source} className="cardImage"/>
+
+            <div className="textContainer">
+                <h1>{props.title}</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+            </div>
+
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Navbar from "../components/navbar";
 import Card from "../components/home/card";
 import "./index.css";
+import bioPhoto from "../images/bioGang.png";
 // import LogoPlaceholder from "../images/LogoPlaceholder.svg"
+
 
 import * as React from "react";
 
@@ -11,10 +13,11 @@ const home = () => {
             <Navbar />
             <img src="https://static.igem.wiki/teams/4642/wiki/logos/project/light-blue-animated.webp" className="logo"/>
             <section className="container">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card title="Biology" source={bioPhoto}/>
+                <Card title="Hardware" source={"https://static.igem.wiki/teams/4642/wiki/homepage/software.webp"}/>
+                <Card title="Software" source={"https://static.igem.wiki/teams/4642/wiki/homepage/software.webp"}/>
+                <Card title="Human Practices" source={"https://static.igem.wiki/teams/4642/wiki/homepage/software.webp"}/>
+                <Card title="Modelling" source={"https://static.igem.wiki/teams/4642/wiki/homepage/software.webp"}/>
             </section>
         </>
     )

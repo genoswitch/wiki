49de3cdbf809cd87bcd8df4055deee830e1afd84
James Cahill <jcahill773@gmail.com>
2023-10-02 15:16:19 +0100

    Use temp folder to fix copying to/from self

--- post-build-prod.sh
+++ post-build-prod.sh
@@ -6,9 +6,10 @@
 # Src: https://documentation.platformos.com/use-cases/using-gatsby-with-platformos
 rsync -a -m --include '*/' --exclude '*.html' --exclude "*.xml" --exclude "*.webmanifest" public app
 #rsync -a -v app/public/assets/ app/public && rm -rf app/public/assets && \
-mkdir -p app/assets/v1/public/city-of-london-uk
-mv -f app/public app/assets/v1
-mv app/assets/v1/public/* app/assets/v1/public/city-of-london-uk/
+mkdir -p app/assets_temp app/assets/v1/public/city-of-london-uk
+mv -f app/public app/assets_temp
+
+mv app/assets_temp/public/* app/assets/v1/public/city-of-london-uk/
 
 # Move html files (content) to app/public
 # Src: https://documentation.platformos.com/use-cases/using-gatsby-with-platformos

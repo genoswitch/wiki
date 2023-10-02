b99c1b6ce4fe7b439c0eecc4cc46c05bd1d973ae
James Cahill <jcahill773@gmail.com>
2023-10-02 13:55:27 +0100

    Don't create a new folder for each publish on s3

--- post-build-prod.sh
+++ post-build-prod.sh
@@ -6,8 +6,8 @@
 # Src: https://documentation.platformos.com/use-cases/using-gatsby-with-platformos
 rsync -a -m --include '*/' --exclude '*.html' --exclude "*.xml" --exclude "*.webmanifest" public app
 #rsync -a -v app/public/assets/ app/public && rm -rf app/public/assets && \
-mkdir -p app/assets/$(git rev-parse --verify HEAD)
-mv -f app/public app/assets/$(git rev-parse --verify HEAD)
+mkdir -p app/assets/v1
+mv -f app/public app/assets/v1
 
 # Move html files (content) to app/public
 # Src: https://documentation.platformos.com/use-cases/using-gatsby-with-platformos

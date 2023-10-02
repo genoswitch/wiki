b9081b676a09de2f29c811c337aa9c52cc05bd8f
Louis Cahill <l.cahill@cityoflondonschool.org.uk>
2023-10-02 21:44:31 +0100

    Homepage (HP & Hardware) and Bio Safety Update (#180)
    * Added text to HP and Hardware Homepage card
    
    * added Biology safety table
    
    * (lint): Run prettier against 1b9fe68c6017c37f3f877e671e6d3a0afa56e4bd
    
    * syntax fix
    
    * (lint): Run prettier against d7534c0bdb36b4f7d4c655f81e2ba6498c0dcf0a
    
    * Added general lab safety
    
    * (lint): Run prettier against e1424572aecf1e4c53be498df0a9a82ed1dfb289
    
    * added more spacing
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/pages/biology/safety.tsx
+++ src/pages/biology/safety.tsx
@@ -38,6 +38,101 @@ export default class BiologySafety extends React.Component {
 				"Wear suitable protective clothing, and wash hands and other exposed areas before exiting the work area. Work in an area of good ventilation.",
 			infoSource: "http://www.labchem.com/tools/msds/msds/LC24450.pdf",
 		},
+		{
+			name: "Agarose",
+			riskHazards: "Irritant to skin and eyes, do not ingest.",
+			precautions: "Wear PPE, ensure adequate ventilation.",
+			infoSource:
+				"https://www.fishersci.co.uk/store/msds?partNumber=10776644&productDescription=500GR+Agarose+broad+separation+range+for+DNA%2FRNA%2CGenetic+Analysis+Grade&countryCode=GB&language=en",
+		},
+		{
+			name: "Bacto-Tryptone",
+			riskHazards: "No hazards, although care should be taken.",
+			precautions: "No special measures required, but care should be taken.",
+			infoSource:
+				"https://uwm.edu/bms-labs/wp-content/uploads/sites/361/2016/01/Bacto-Tryptone-211705.pdf",
+		},
+		{
+			name: "E. coli",
+			riskHazards: "No specific hazards, although care must be taken.",
+			precautions: "Wear PPE, wash hands and exposed areas. Ensure adequate ventilation.",
+			infoSource:
+				"https://www.fishersci.co.uk/store/msds?partNumber=11424109&productDescription=X50+WELLCOLEX+E.+COLI+O157&countryCode=GB&language=en",
+		},
+		{
+			name: "KCl",
+			riskHazards: "No specific hazard.",
+			precautions: "Wear PPE, ensure adequate ventilation, and avoid ingestion.",
+			infoSource:
+				"https://www.fishersci.co.uk/store/msds?partNumber=10684732&productDescription=1kg,%20Plastic%20powder%20jar&countryCode=GB&language=en",
+		},
+		{
+			name: "NaCl",
+			riskHazards: "No specific hazard.",
+			precautions: "Wear PPE and avoid ingestion, along with adequate ventilation.",
+			infoSource:
+				"https://www.fishersci.co.uk/store/msds?partNumber=10112640&productDescription=1KG+Sodium+chloride%2C+extra+pure%2C+SLR&countryCode=GB&language=en",
+		},
+		{
+			name: "Virkon",
+			riskHazards: "Skin irritant, serious eye damage. Respiratory irritant.",
+			precautions:
+				"Do not ingest, do not get on skin or in eyes, only work in an area with adequate ventilation. Wash hands and exposed areas.",
+			infoSource: "https://www.cvear.com/wp-content/uploads/2020/02/Virkon-SDS.pdf",
+		},
+		{
+			name: "β-mercaptoethanol",
+			riskHazards: "Skin irritant, Serious eye damage. Avoid ingestion at serious risk.",
+			precautions:
+				"Wear PPE, do not get on skin or in eyes. Use with adequate ventilation, usually a fume hood.",
+			infoSource:
+				"https://www.fishersci.co.uk/chemicalProductData_uk/wercs?itemCode=10366313&lang=EN",
+		},
+		{
+			name: "5-bromo-4-chloro-3- indolyl-β-D-galactopyranoside",
+			riskHazards: "No serious hazard, or recorded hazard.",
+			precautions: "Wear appropriate clothing, take care when handling and wash hands after use.",
+			infoSource: "https://www.affymetrix.com/support/technical/usb/msds/10077A.pdf",
+		},
+		{
+			name: "Dimethylformamide",
+			riskHazards: "Flammable, skin irritant, serious eye damage, and respiratory irritant.",
+			precautions:
+				"Wear PPE and use under fume hood in general. Avoid getting in eyes or on skin, and wash hands and exposed skin after use.",
+			infoSource:
+				"https://www.fishersci.co.uk/store/msds?partNumber=10757894&productDescription=1LT+Dimethylformamide%2C+extra+pure%2C+SLR&countryCode=GB&language=en",
+		},
+		{
+			name: "Isopropyl-1-thio-β-D'Galactopyranoside",
+			riskHazards: "No serious hazard, but care should be taken.",
+			precautions:
+				"Wear PPE, and avoid breathing fumes. Do not ingest, and only use in areas of ventilation.",
+			infoSource:
+				"https://www.fishersci.com/store/msds?partNumber=BP175510&productDescription=IPTG+DIOXANE+FREE+10GR&vendorId=VN00033897&countryCode=US&language=en",
+		},
+		{
+			name: "1M MgCl2",
+			riskHazards: "No serious hazard, although care should be taken.",
+			precautions:
+				"Wear PPE, and avoid ingestion or inhalation. Use in an area with adequate ventilation.",
+			infoSource:
+				"https://www.fishersci.com/store/msds?partNumber=AAJ62411AD&productDescription=MAG+CHLORIDE+1M+AQ.+SOLN.+50ML&vendorId=VN00024248&countryCode=US&language=en",
+		},
+		{
+			name: "1M MgSO4",
+			riskHazards: "Skin and eye irritant. May be harmful if ingested or inhaled.",
+			precautions:
+				"Wear PPE, and avoid contact with skin and eyes. Don't ingest or inhale. Wash hands and exposed areas after use.",
+			infoSource: "https://fscimage.fishersci.com/msds/13510.htm",
+		},
+		{
+			name: "Yeast Extract",
+			riskHazards: "No serious hazards, although care should be taken.",
+			precautions:
+				"Avoid inhaling, avoid contact with skin and eyes. Wash hands and exposed areas after handling.",
+			infoSource:
+				"https://www.fishersci.com/content/dam/fishersci/en_US/documents/programs/education/regulatory-documents/sds/chemicals/chemicals-y/S25631.pdf",
+		},
 	];
 
 	render(): React.ReactNode {
@@ -45,8 +140,27 @@ export default class BiologySafety extends React.Component {
 			<HeaderFooterProvider>
 				<div style={{ padding: 16 }}>
 					<Typography variant="h3">Biology Safety</Typography>
-
-					<Typography variant="h4">Project Safety Assessment</Typography>
+					<div style={{ padding: 8 }}></div>
+					<Typography variant="h4">General Laboratory Safety</Typography>
+					<ul>
+						<li>
+							Gloves were worn at all times throughout all procedures being disposed of responsibly
+							on departure.
+						</li>
+						<li>
+							Lab coats were worn at all times and were clean, then returned to be washed following
+							protocols.
+						</li>
+						<li>Goggles were worn when handling chemicals or other equipment.</li>
+						<li>Long trousers were worn to protect legs.</li>
+						<li>Closed toe shoes worn when in the lab.</li>
+						<li>
+							All belongings were left in a designated area and any necessary personal items were
+							left on a designated bench which was not used for any protocols.
+						</li>
+					</ul>
+					<div style={{ padding: 16 }}></div>
+					<Typography variant="h4">Laboratory Chemicals Safety Assessment</Typography>
 					<TableContainer component={Paper}>
 						<Table sx={{ minWidth: 650 }} aria-label="simple table">
 							<TableHead>

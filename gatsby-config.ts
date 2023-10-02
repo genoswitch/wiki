d20bbebac24e71405d9ccbd8b188a99ccf12d9aa
James Cahill <jcahill773@gmail.com>
2023-10-02 14:35:56 +0100

    update default assetPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,7 +13,8 @@ const config: GatsbyConfig = {
 		sha: childProcess.execSync("git rev-parse --verify HEAD").toString().trim(),
 	},
 	pathPrefix: process.env.PATH_PREFIX || "/city-of-london-uk",
-	assetPrefix: process.env.ASSET_PREFIX || "https://static.igem.wiki/teams/4642/wiki/",
+	assetPrefix:
+		process.env.ASSET_PREFIX || "https://static.igem.wiki/teams/4642/wiki/assets/v1/public",
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen

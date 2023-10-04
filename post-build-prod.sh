0f9dc5d7fae938c3e77c2702527d949b0e57a9ae
James Cahill <jcahill773@gmail.com>
2023-10-04 15:12:21 +0100

    (postbuild): Misc fixes

--- post-build-prod.sh
+++ post-build-prod.sh
@@ -3,5 +3,5 @@
 # Moves public/static to static, replaces all instances of /city-of-london-uk/static with static.igem.wiki url.
 
 mkdir -p static/assets/v2
-mv public/static static/assets/v2
-find ./public -type f -exec sed -i "s/\/city-of-london-uk\/static\//https:\/\/static.igem.wiki\/teams\/4642\/wiki\/assets\/v2\//g" {} \;
+mv public/static/ static/assets/v2
+find ./public -type f -exec sed -i "s/\/city-of-london-uk\/static\//https:\/\/static.igem.wiki\/teams\/4642\/wiki\/assets\/v2\/static\//g" {} \;

6628c8c344cf6c7c75a08898398f20beacf90dec
James Cahill <jcahill773@gmail.com>
2023-10-04 20:03:05 +0100

    (lint): Lint src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,8 +64,9 @@ export default class Homepage extends React.Component<
 							src="https://static.igem.wiki/teams/4642/wiki/logos/team/logo-team-project-black.svg"
 							style={{
 								width: "50%",
-								filter: "invert(99%) sepia(29%) saturate(4144%) hue-rotate(155deg) brightness(91%) contrast(90%)",
-								paddingBottom: "100px"
+								filter:
+									"invert(99%) sepia(29%) saturate(4144%) hue-rotate(155deg) brightness(91%) contrast(90%)",
+								paddingBottom: "100px",
 							}}
 							class="animate__animated animate__fadeInLeftBig animate__delay-3s"
 						/>

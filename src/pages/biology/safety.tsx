d6ab32659062ab1002f37f9e45a54123d3097b71
Louis Cahill <l.cahill@cityoflondonschool.org.uk>
2023-10-05 10:39:50 +0100

    Added Lab Safety in Progress (#190)
    * Added Lab Safety in Progress
    
    * (lint): Run prettier against eed032981deda5960620062f6ffb4c9139b7efe9
    
    * Added spacing to Safety in Practice
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/pages/biology/safety.tsx
+++ src/pages/biology/safety.tsx
@@ -161,6 +161,24 @@ export default class BiologySafety extends React.Component {
 						</li>
 					</ul>
 					<div style={{ padding: 16 }}></div>
+					<h2>Laboratory Safety in Practice</h2>
+					As a high school team, we are not as used to working in laboratories as other teams, and
+					so throughout the process, we we have learnt so much about lab safety in general,
+					especially the heightened precautions when working in a university lab in comparison to a
+					school lab.
+					<div style={{ padding: 12 }}></div>
+					<img src="https://static.igem.wiki/teams/4642/wiki/biology/safety/biology-safety-1.jpg" />
+					Despite a rocky start, with a dirty lab coat in tow we soon began to pick up the
+					requirements necessary to successfully adhere to correct lab protocol
+					<div style={{ padding: 12 }}></div>
+					<img src="https://static.igem.wiki/teams/4642/wiki/biology/safety/biology-safety-2.jpg" />
+					With quick learning being a strong point, we adapted rapidly to the new environment and a
+					freshly washed lab coat was soon donned.
+					<div style={{ padding: 12 }}></div>
+					<img src="https://static.igem.wiki/teams/4642/wiki/biology/safety/biology-safety-3.jpg" />
+					Further progression was made with some skillful glove application, all that remained
+					lacking was long trousers which was amended with our next trip to the lab.
+					<div style={{ padding: 16 }}></div>
 					<h2>Laboratory Chemicals Safety Assessment</h2>
 					<TableContainer component={Paper}>
 						<Table sx={{ minWidth: 650 }} aria-label="simple table">

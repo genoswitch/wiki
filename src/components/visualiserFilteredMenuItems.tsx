693e814c710e70e53276983c38f453b2f695763d
James Cahill <jcahill773@gmail.com>
2023-10-08 17:15:27 +0100

    (visualiser): Revert back to mui-nested-menu

--- src/components/visualiserFilteredMenuItems.tsx
+++ src/components/visualiserFilteredMenuItems.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { MenuItem } from "@material-tailwind/react";
+import { MenuItem } from "@mui/material";
 
 import { SequenceDefinitionNode } from "../types/graphql/sequenceDefintionNode";
 import { SequenceTag } from "../types/sequenceTag";

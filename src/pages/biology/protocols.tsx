4818547a1a7f943a2cf9c39452765e57a839cdcb
Louis Cahill <l.cahill@cityoflondonschool.org.uk>
2023-10-10 19:56:11 +0100

    Fixed Biology Protocols Page (#227)
    * Added summary list of protocols
    
    * Added Biology Prep to the protocols tab
    
    * (lint): Run prettier against 2b1613be8bfa0414bef6af5bb20344319113af4a
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/pages/biology/protocols.tsx
+++ src/pages/biology/protocols.tsx
@@ -15,9 +15,20 @@ export default class BiologyProtocolsPage extends React.Component {
 						In this section, you will find all the protocols that we used during our time in the
 						labs.
 					</b>
-				<br/>
-					Please click through the tabs below to look at each of our protocols.
-				<br/>
+					<br />
+					Please click through the tabs below to look at each of our protocols:
+					<ul>
+						<li>Stock solutions for creating competent bacteria</li>
+						<li>Antibiotic Stock Solutions</li>
+						<li>Pouring agar Plates</li>
+						<li>Preparing Cells</li>
+						<li>Making Competent Cells</li>
+						<li>E. coli Transformation</li>
+						<li>Determination of bacterial optical density (OD) using the spectrophotometer"</li>
+						<li>Conducting Gel Electrophoresis</li>
+						<li>PCR Preparations</li>
+					</ul>
+					<br />
 					<BiologyProtocols />
 				</Container>
 			</HeaderFooterProvider>

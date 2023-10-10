a417e43ba21758635727969d8416be00049e77ff
Louis Cahill <lcahill2711@gmail.com>
2023-10-10 18:49:32 +0000

    Added summary list of protocols

--- src/pages/biology/protocols.tsx
+++ src/pages/biology/protocols.tsx
@@ -16,7 +16,18 @@ export default class BiologyProtocolsPage extends React.Component {
 						labs.
 					</b>
 				<br/>
-					Please click through the tabs below to look at each of our protocols.
+					Please click through the tabs below to look at each of our protocols:
+					<ul>
+						<li>Stock solutions for creating competent bacteria</li>
+						<li>Antibiotic Stock Solutions</li>
+						<li>Pouring agar Plates</li>
+						<li>Preparing Cells</li>
+						<li>Making Competent Cells</li>
+						<li>E. coli Transformation</li>
+						<li>Determination of bacterial optical density (OD) using the spectrophotometer"</li>
+						<li>Conducting Gel Electrophoresis</li>
+						<li>PCR Preparations</li>
+					</ul>
 				<br/>
 					<BiologyProtocols />
 				</Container>
